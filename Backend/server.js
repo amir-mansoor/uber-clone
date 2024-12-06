d4e96faea41fb48669847f301219f580a6d62727
amir-mansoor <qazimansor89@gmail.com>
2024-12-06 09:46:09 +0500

    fix the bugs in the code

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const app = require('./app');
-const { initializeSocket } = require('./socket');
+const http = require("http");
+const app = require("./app.js");
+const { initializeSocket } = require("./socket.js");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
@@ -8,5 +8,5 @@ const server = http.createServer(app);
 initializeSocket(server);
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
